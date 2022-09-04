89b6f6c441391b5273af3f8c62e009334eb1911b
Theodor Cárdenas <theodorcardenas@gmail.com>
2022-09-03 19:04:46 -0500

    Update view services osa

--- app/Http/Livewire/StatusServicesOsa.php
+++ app/Http/Livewire/StatusServicesOsa.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Site;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -11,9 +12,12 @@ class StatusServicesOsa extends Component
     public $itemId;
     public $servicesOsa;
     public $readyToLoad = false;
+    public $sid;
 
     public function mount($customerId, $itemId){
-        $this->customerId=$customerId;
+        $sid = Site::where('customer_id',$customerId)->first();
+        $this->sid=$sid['sid'];
+        $this->customerId = $customerId;
         $this->itemId=$itemId;
     }
 
@@ -25,32 +29,33 @@ class StatusServicesOsa extends Component
     public function render()
     {
         $cases = $this->readyToLoad
-            ? collect($this->getServices($this->customerId, $this->itemId))
+            ? collect($this->getServices($this->customerId, $this->itemId, $this->sid))
             : [];
         return view('livewire.status-services-osa', compact('cases'));
     }
 
-    public function test($customerId, $itemId){
+    /*public function test($customerId, $itemId){
         $data = Http::retry(10, 500)
             ->withCookies(["connect.sid"=>env('SID')],"dcc.ext.hp.com")
             ->get('https://dcc.ext.hp.com/ui/service-orders/list?customerId='.$customerId.'&itemId='.$itemId.'&kits=false');
         return $data;
-    }
+    }*/
 
-    public function getServices($customerId, $itemId){
+    public function getServices($customerId, $itemId, $sid){
         $case_osa = [];
         $data = Http::retry(10, 500)
-            ->withCookies(["connect.sid"=>env('SID')],"dcc.ext.hp.com")
+            ->withCookies(
+                ["connect.sid"=>$sid],
+                "dcc.ext.hp.com"
+            )
             ->get('https://dcc.ext.hp.com/ui/service-orders/list?customerId='.$customerId.'&itemId='.$itemId.'&kits=false');
 
         if($data->successful()){
-            $item = $data->json();
-            dd($item);
-            if(isset($item['rows'])){
-                $case_osa=collect($item);
-                $case_osa->put('count',count($item['rows']));
+            if(isset($data['rows'])){
+                $case_osa=collect($data);
+                $case_osa->put('count',count($data['rows']));
 
-                $rows = collect($item['rows']);
+                $rows = collect($data['rows']);
                 $case_osa->put('open', $rows->where('open', true)->count());
                 $case_osa->put('close', $rows->where('open', false)->count());
             }
