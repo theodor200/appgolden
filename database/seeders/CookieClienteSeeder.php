e90ebb67affbca7effd16137a2527d826bf8065a
theodorcardenas@gmail.com <theodorcardenas@gmail.com>
2023-11-04 23:17:57 -0500

    Session GLPI

--- database/seeders/CookieClienteSeeder.php
+++ database/seeders/CookieClienteSeeder.php
@@ -17,7 +17,7 @@ class CookieClienteSeeder extends Seeder
     {
         DB::table('glpi')->insert(
             [
-                'cookie'=>'NULL'
+                'cookie'=>'glpi_a39671d191afef61072046348dab714b=j38aeu9h21dps0kbd47shkc13v; glpi_a39671d191afef61072046348dab714b_rememberme=%5B11488%2C%2281Pji7BXwhrUw8DabuZC9wnUmXUzhdSE9CuyTe01%22%5D'
             ]
         );
     }
